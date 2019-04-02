eedc849012e42023a21c1969ce96bf67da135326
Henry Grover <groveh@rpi.edu>
2019-04-02 19:35:16 -0400

    steady progress

--- assignment4-5.c
+++ assignment4-5.c
@@ -59,7 +59,7 @@ Randomness: random value for every cell each tick, if below some threshold,
 #define ALIVE 1
 #define DEAD  0
 
-#define board_size 4//32768
+#define board_size 8//32768
 
 /***************************************************************************/
 /* Global Vars *************************************************************/
@@ -131,70 +131,122 @@ int main(int argc, char *argv[])
         int rows_per_thread = rows_per_rank/num_threads;
         printf("each of %d threads will have %d cells, %d rows\n",
             num_threads,cells_per_thread, rows_per_thread);
+    }
 
-        // attempt to allocate dynamically in other functions (didnt work)
-        // int h=2, w=2;
-        // int ** board;
-        // allocate_mem(&board, h, w);
-        // print_board(&board, h, w);
-        
-        // declare main variables
-        int h = 3; 
-        int **board;
-        int *ghost_above;
-        int *ghost_below;
+    // attempt to allocate dynamically in other functions (didnt work)
+    // int h=2, w=2;
+    // int ** board;
+    // allocate_mem(&board, h, w);
+    // print_board(&board, h, w);
 
-        // allocate memory for the board
-        board = (int **)malloc(h * sizeof(int *)); 
-        for (int i=0; i<h; i++){
-            board[i] = (int *)malloc(board_size * sizeof(int)); 
+    /*  1
+        allocate my rank's chunk of the universe + space for ghost rows
+            ghost rows are the rows at edges of rank boundaries
+            allocating 2d array of the board for each rank
+    */
+    // allocate dynamically within main bc in functions was taking up time
+    // declare main variables
+    int num_ticks = 10;
+    int rows_per_rank = board_size/mpi_commsize; 
+    int rows_per_thread = rows_per_rank/num_threads;
+    int **board;
+    int *ghost_above;
+    int *ghost_below;
+
+    // allocate memory for the board
+    board = (int **)malloc(rows_per_rank * sizeof(int *)); 
+    for (int i=0; i<rows_per_rank; i++){
+        board[i] = (int *)malloc(board_size * sizeof(int)); 
+    }
+    // allocate memory for the two ghost rows
+    ghost_above = (int *)malloc(board_size * sizeof(int)); 
+    ghost_below = (int *)malloc(board_size * sizeof(int)); 
+    
+    // initialize the board
+    for(int i=0; i<rows_per_rank; i++){
+        for(int j=0; j<board_size; j++){
+            board[i][j] = ALIVE;
         }
-        // allocate memory for the two ghosts
-        ghost_above = (int *)malloc(board_size * sizeof(int)); 
-        ghost_below = (int *)malloc(board_size * sizeof(int)); 
-        
-        // initialize the board
-        for(int i=0; i<h; i++){
-            for(int j=0; j<board_size; j++){
-                board[i][j] = ALIVE;
-            }
-        }
-        // initialize the ghost row above
-        for(int i=0; i<board_size; i++){
-            ghost_above[i] = ALIVE;
-        }
-        // initialize the ghost row below
-        for(int i=0; i<board_size; i++){
-            ghost_below[i] = ALIVE;
-        }
-      
-        // print the board
-        for(int i=0; i<h; i++){
-            for(int j=0; j<board_size; j++){ 
-                printf("%d ", board[i][j]); 
-            }
-            printf("\n");
-        }
-        printf("\n");
-        // print the ghost row above
-        for(int i=0; i<board_size; i++){ 
-            printf("%d ", ghost_above[i]); 
-        }
-        printf("\n");
-        // print the ghost row below
-        for(int i=0; i<board_size; i++){ 
-            printf("%d ", ghost_below[i]); 
-        }
-        printf("\n");
+    }
+    // initialize the ghost row above
+    for(int i=0; i<board_size; i++){
+        ghost_above[i] = ALIVE;
+        ghost_below[i] = ALIVE;
+    }
+  
+
+    // // print the board for sanity
+    // for(int i=0; i<rows_per_rank; i++){
+    //     for(int j=0; j<board_size; j++){ 
+    //         printf("%d ", board[i][j]); 
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    // // print the ghost row above
+    // for(int i=0; i<board_size; i++){ 
+    //     printf("%d ", ghost_above[i]); 
+    // }
+    // printf("\n");
+    // // print the ghost row below
+    // for(int i=0; i<board_size; i++){ 
+    //     printf("%d ", ghost_below[i]); 
+    // }
+    // printf("\n");
+
+    /*  2
+        Create Pthreads here. All threads should go into for loop
+    */
+
+
+
+
+
+    /*  3
+        For all number of ticks, complete a round of the GOL
+    */
+    for(int tick=0; tick<num_ticks; tick++){
+
+        /*  4
+            Exchange row data with MPI ranks 
+            using MPI_Isend/Irecv from thread 0 w/i each MPI rank.
+            Yes, you must correctly MPI_Test or Wait to make sure
+            messages operations correctly complete.
+
+            [Note: have only 1 MPI rank/pthread perform ALL MPI
+            operations per rank/thread group. Dont’ allow multiple
+            threads to perform any MPI operations within MPI
+            rank/thread group.]
+
+        */  
+
+
+
+        /* 
+
+        */
+
+
+
+
+
+
+
+
 
 
 
 
     }
+
+    // Deallocate memory
     
-    // allocate my rank's chunk of the universe + space for ghost rows
-    	// ghost rows are the rows at edges of rank boundaries
-        // allocating 2d array of the board for each rank
+    for (int i = 0; i<h; i++){
+        free(board[i]);
+    }
+    free(arr); 
+    free(ghost_above);
+    free(ghost_below);
 
     
     
