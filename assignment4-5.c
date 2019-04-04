5b13e4528f078832fac78e53f8bf760c2a6caa01
Austin Egri <ategri@mac.com>
2019-04-03 21:45:33 -0400

    update

--- assignment4-5.c
+++ assignment4-5.c
@@ -87,7 +87,7 @@ pthread_mutex_unlock( &mutex );
 /*******************************/
  
  
- double g_time_in_secs = 0;
+double g_time_in_secs = 0;
 double g_processor_frequency = 1600000000.0; // processing speed for BG/Q
 unsigned long long g_start_cycles=0;
 unsigned long long g_end_cycles=0;
